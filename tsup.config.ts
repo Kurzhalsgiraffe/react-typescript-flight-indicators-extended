e09f355ba5cfa46aa6716c57a9a24b88ea2a8ac5
Benjamin <benjamin@pogacic.com>
2025-08-30 23:54:53 +0100

    Adjust tsup config for esm and cjs module

---
+++ tsup.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig([
+  {
+    entryPoints: ['src/index.tsx',],
+    format: ['cjs', 'esm'],
+    dts: true,
+    minify: false,
+    outDir: 'dist/',
+    clean: true,
+    sourcemap: false,
+    bundle: true,
+    splitting: false,
+    outExtension (ctx) {
+      return {
+        dts: '.d.ts',
+        js: ctx.format === 'cjs' ? '.cjs' : '.mjs',
+      }
+    },
+    treeshake: false,
+    target: 'es2020',
+    platform: 'node',
+    tsconfig: './tsconfig.json',
+    cjsInterop: true,
+    keepNames: true,
+    skipNodeModulesBundle: false,
+  },
+])
